6c4a6824920b17027af05b8cb9ddad9f17347b98
Monami Yoshioka <monami.yoshioka@gmail.com>
2024-01-28 12:35:48 +0000

    Install GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 using namespace std; 
@@ -6,25 +8,26 @@ using namespace std;
 int main()
 {
     if (!glfwInit())
-    {
-        cout << "Failed to initialize GLFW" << endl;
         return -1;
-    }
-
-    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
-    if (window == NULL)
-    {
-        cout << "Failed to open GLFW window" << endl;
+    window = glfwCreateWindow(800, 600, "Space Exploration", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     while(!glfwWindowShouldClose(window))
     {
